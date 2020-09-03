f3a42394cfbfa694ec786d8572c431a4781b8058
vividswan <vividswan@naver.com>
2020-09-04 03:09:26 +0900

    Update UserService.java

--- UserService/src/userService/gui/UserService.java
+++ UserService/src/userService/gui/UserService.java
@@ -12,8 +12,12 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.RowFilter;
 import javax.swing.SwingConstants;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 import userService.serviceLayer.ServiceLayer;
 import userService.vo.UserVO;
@@ -23,9 +27,12 @@ public class UserService {
 	ServiceLayer serviceLayer = ServiceLayer.getInstance();
 	private JFrame frame;
 	private JTextField name;
-	private JTextField email;
-	private JTextField age;
+	private JTextField birth;
+	private JTextField number;
 	private JTextField findName;
+	private JTextField modifyNum;
+	private JTextField modifyId;
+	private JTextField deleteId;
 
 	/**
 	 * Launch the application.
@@ -55,7 +62,7 @@ public class UserService {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 784, 487);
+		frame.setBounds(100, 100, 784, 615);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -65,32 +72,32 @@ public class UserService {
 		NameLabel.setBounds(75, 27, 62, 18);
 		frame.getContentPane().add(NameLabel);
 		
-		JLabel EamilLabel = new JLabel("email :");
-		EamilLabel.setBounds(262, 27, 62, 18);
-		frame.getContentPane().add(EamilLabel);
+		JLabel BirthLabel = new JLabel("birth :");
+		BirthLabel.setBounds(270, 27, 62, 18);
+		frame.getContentPane().add(BirthLabel);
 		
-		JLabel AgeLabel = new JLabel("age :");
-		AgeLabel.setHorizontalAlignment(SwingConstants.TRAILING);
-		AgeLabel.setBounds(419, 27, 62, 18);
-		frame.getContentPane().add(AgeLabel);
+		JLabel NumberLabel = new JLabel("number :");
+		NumberLabel.setHorizontalAlignment(SwingConstants.TRAILING);
+		NumberLabel.setBounds(444, 27, 62, 18);
+		frame.getContentPane().add(NumberLabel);
 		
 		name = new JTextField();
 		name.setBounds(124, 24, 124, 24);
 		frame.getContentPane().add(name);
 		name.setColumns(10);
 		
-		email = new JTextField();
-		email.setBounds(311, 24, 116, 24);
-		frame.getContentPane().add(email);
-		email.setColumns(10);
+		birth = new JTextField();
+		birth.setBounds(311, 24, 116, 24);
+		frame.getContentPane().add(birth);
+		birth.setColumns(10);
 		
-		age = new JTextField();
-		age.setBounds(490, 24, 116, 24);
-		frame.getContentPane().add(age);
-		age.setColumns(10);
+		number = new JTextField();
+		number.setBounds(510, 24, 116, 24);
+		frame.getContentPane().add(number);
+		number.setColumns(10);
 		
 		JButton save = new JButton("\uB4F1\uB85D");
-		save.setBounds(620, 23, 105, 27);
+		save.setBounds(640, 23, 105, 27);
 		frame.getContentPane().add(save);
 		
 		JLabel FindNameLabel = new JLabel("Find Name :");
@@ -108,64 +115,143 @@ public class UserService {
 		frame.getContentPane().add(find);
 		
 		JButton findAll = new JButton("\uC804\uCCB4 \uD68C\uC6D0 \uBAA9\uB85D");
-		findAll.setBounds(311, 126, 159, 27);
+		findAll.setBounds(311, 228, 159, 27);
 		frame.getContentPane().add(findAll);
 		
+		JTextArea ta = new JTextArea();
+		ta.setBounds(180, 267, 446, 301);
+		frame.getContentPane().add(ta);
 		
-		String[][] data = serviceLayer.userList();
-		String[] header = new String[] {"회원번호","이름","이메일","나이"};
-		JPanel findAllP = new JPanel();
-		findAllP.setBounds(108, 165, 588, 251);
-		JTable table = new JTable(data,header);
-		table.setRowHeight(30);
-		table.setAlignmentX(0);
-		table.setSize(550,400);
-		table.setPreferredScrollableViewportSize(new Dimension(550,400));
+		modifyNum = new JTextField();
+		modifyNum.setBounds(401, 127, 116, 24);
+		frame.getContentPane().add(modifyNum);
+		modifyNum.setColumns(10);
 		
-		findAllP.add(new JScrollPane(table));
-		frame.getContentPane().add(findAllP);
+		modifyId = new JTextField();
+		modifyId.setBounds(180, 127, 116, 24);
+		frame.getContentPane().add(modifyId);
+		modifyId.setColumns(10);
 		
-		String[][] data2 = serviceLayer.findByName(findName.getText());
-		String[] header2 = new String[] {"회원번호","이름","이메일","나이"};
-		JPanel findNameP = new JPanel();
-		findNameP.setBounds(108, 165, 588, 251);
-		JTable table2 = new JTable(data2,header2);
-		table2.setRowHeight(30);
-		table2.setAlignmentX(0);
-		table2.setSize(550,400);
-		table2.setPreferredScrollableViewportSize(new Dimension(550,400));
+		JLabel numberLabel2 = new JLabel("number :");
+		numberLabel2.setHorizontalAlignment(SwingConstants.TRAILING);
+		numberLabel2.setBounds(325, 130, 62, 18);
+		frame.getContentPane().add(numberLabel2);
 		
-		findNameP.add(new JScrollPane(table2));
-		frame.getContentPane().add(findNameP);
+		JLabel idLabel = new JLabel("ID :");
+		idLabel.setBounds(134, 130, 62, 18);
+		frame.getContentPane().add(idLabel);
+		
+		JButton modifyBtn = new JButton("\uC218\uC815");
+		modifyBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		modifyBtn.setBounds(559, 126, 105, 27);
+		frame.getContentPane().add(modifyBtn);
+		
+		JLabel label = new JLabel("ID :");
+		label.setBounds(292, 174, 62, 18);
+		frame.getContentPane().add(label);
+		
+		deleteId = new JTextField();
+		deleteId.setBounds(335, 171, 116, 24);
+		frame.getContentPane().add(deleteId);
+		deleteId.setColumns(10);
+		
+		JButton deleteBtn = new JButton("\uC0AD\uC81C");
+		deleteBtn.setBounds(477, 170, 105, 27);
+		frame.getContentPane().add(deleteBtn);
+		
+				
+
+		
+
+		
+
+		
+		findAll.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+				ta.setText("");
+				String[][] arr = serviceLayer.userList();
+				
+				ta.append("number"+"\t"+"name"+"\t"+"birth date"+"\t"+"phone number"+"\n");
+				ta.append("----------------------------------------------------------------------------------------------------------\n");
+				
+				for (int i = 0; i < arr.length; i++) {
+					ta.append(arr[i][0]+" \t "+ arr[i][1]+" \t "+arr[i][2]+" \t "+arr[i][3]+" \t "+ "\n");
+				}
+				
+			}
+		});
+		
+		
+		
 		
 		find.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				findAllP.setVisible(false);
-				//findNameP.setVisible(true);
+			public void actionPerformed(ActionEvent e) {
+				String val = findName.getText();
+				
+				ta.setText("");
+				String[][] arr = serviceLayer.findByName(val);
+				
+				ta.append("number"+"\t"+"name"+"\t"+"birth date"+"\t"+"phone number"+"\n");
+				ta.append("----------------------------------------------------------------------------------------------------------\n");
+				
+				for (int i = 0; i < arr.length; i++) {
+					ta.append(arr[i][0]+" \t "+ arr[i][1]+" \t "+arr[i][2]+" \t "+arr[i][3]+" \t "+ "\n");
+				}
 			}
 		});
 		
 		save.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				String nameValue = name.getText();
-				String emailValue = email.getText();
-				String ageValue = age.getText();
+				String birthValue = birth.getText();
+				String numberValue = number.getText();
 				
-				if( nameValue.isEmpty() || emailValue.isEmpty() || ageValue.isEmpty()) {
+				if( nameValue.isEmpty() || birthValue.isEmpty() || numberValue.isEmpty()) {
 					JOptionPane.showMessageDialog(null, "모두 작성해주세요.");
 				}
 				else {
-					findAllP.setVisible(false);
 					UserVO user = new UserVO();
 					user.setName(nameValue);
-					user.setEmail(emailValue);
-					user.setAge(Integer.parseInt(ageValue));
+					user.setBirth(Integer.parseInt(birthValue));
+					user.setNumber(numberValue);
 					
 					ServiceLayer serviceLayer = ServiceLayer.getInstance();
 					serviceLayer.userInsert(user);
-					
-					
-					findAllP.setVisible(true);
+									
+				}
+			}
+		});
+		
+		modifyBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String idValue = modifyId.getText();
+				String numberValue = modifyNum.getText();
+				
+				if( idValue.isEmpty() || numberValue.isEmpty()) {
+					JOptionPane.showMessageDialog(null, "모두 작성해주세요.");
+				}
+				else {
+					ServiceLayer serviceLayer = ServiceLayer.getInstance();
+					serviceLayer.update(Integer.parseInt(idValue), numberValue);
+				}
+			}
+		});
+		
+		deleteBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String idValue = deleteId.getText();
+				
+				if( idValue.isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Id를 작성해주세요.");
+				}
+				else {
+					ServiceLayer serviceLayer = ServiceLayer.getInstance();
+					serviceLayer.delete(Integer.parseInt(idValue));
 				}
 			}
 		});
