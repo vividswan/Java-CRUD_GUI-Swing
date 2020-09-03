9dcdbe65d0a3df8f7d1f5e90f0dd29b1655294d9
vividswan <vividswan@naver.com>
2020-09-03 20:51:00 +0900

    Update UserService.java

--- UserService/src/userService/service/UserService.java
+++ UserService/src/userService/service/UserService.java
@@ -1,5 +1,27 @@
 package userService.service;
 
-public class UserService {
+import java.util.ArrayList;
 
+import userService.dao.UserDAO;
+import userService.vo.UserVO;
+
+public class UserService {
+	private static UserService service = new UserService();
+	
+	public UserDAO dao = UserDAO.getInstance();
+	
+	private UserService() {}
+	private static UserService getInstance() {
+		return service;
+	}
+	
+	public boolean UserInsert(UserVO user) {
+		dao.userInsert(user);
+		return true;
+	}
+	
+	public ArrayList<UserVO> userList(){
+		ArrayList<UserVO> list = dao.userList();
+		return list;
+	}
 }
